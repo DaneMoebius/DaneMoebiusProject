0ce34a4fe7302a2759c9c97e5551fbf8ae0f7422
Dane Moebius <danemoebius@gmail.com>
2016-05-06 14:59:35 -0700

    Added runner and constructor #confused

--- DaneEclipseProject/src/mainpackage/SpeckCipher.java
+++ DaneEclipseProject/src/mainpackage/SpeckCipher.java
@@ -1,5 +1,18 @@
 package mainpackage;
 
 public class SpeckCipher {
+	public String word;
 	
+	public SpeckCipher(String input)
+	{
+		word=input;
+	}
+	public byte encrypt(String input)
+	{
+		return 44;
+	}
+	public String decrypt(byte input)
+	{
+		return "";
+	}
 }
